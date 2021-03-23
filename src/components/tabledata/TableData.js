f9627e2ed9c1ddd6d1278034fbe082d67a5aca36
Oscar Granados <ojgranadosg@gmail.com>
2021-03-22 22:50:11 -0500

    Se agrega datatables

--- src/components/tabledata/TableData.js
+++ src/components/tabledata/TableData.js
@@ -1,8 +1,49 @@
 import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Datatable from 'react-data-table-component';
 
-import TopBrands from './TopBrands';
-import SearchData from './SearchData';
+import TopBrands from './TopBrands'; 
 import { getAlltableData } from '../actions/api';
+
+const columnas = [
+    {
+        name: 'PROPIETARIO',
+        selector: 'ownerName',
+        sortable: true
+    },
+    {
+        name: 'TELEFONO',
+        selector: 'phone',
+        sortable: true
+    },
+    {
+        name: 'CEDULA',
+        selector: 'identification_card',
+        sortable: true
+    },
+    {
+        name: 'CORREO',
+        selector: 'email',
+        sortable: true
+    },
+    {
+        name: 'MARCA',
+        selector: 'brandName',
+        sortable: true
+    },
+    {
+        name: 'PLACA',
+        selector: 'license_plate',
+        sortable: true
+    }
+];
+
+const paginacionOpciones = {
+    rowsPerPageText : 'Filas por pagina',
+    rangeSeparatorText : 'de',
+    selectAllRowsItem: true,
+    selectAllRowsItemText: 'Todos',
+}
 
 const TableData = () => {
 
@@ -19,20 +60,21 @@ const TableData = () => {
     }, [])
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-8">
-                    <SearchData 
-                        data={data}
-                    />
-                </div>
-                <div className="col-md-4">
-                    <TopBrands />
-                </div>
+        <div className="row">
+            <div className="col-md-9">
+                <Datatable 
+                    columns = {columnas}
+                    data = {data}
+                    pagination
+                    paginationComponentOptions = {paginacionOpciones}
+                    title = 'TABLA DE DATOS GENERALES'
+                />
+                <hr></hr>
             </div>
-        </div>  
-        
-
+            <div className="col-md-3 mt-4">
+                <TopBrands />
+            </div>
+        </div>
     );
 }
  
